a18654614ff70fa2986044c73f95850cfc17d981
xiangzhengfeng <eoszhengfeng@163.com>
2022-01-09 18:50:07 +0800

    update converted

--- converted.config.js
+++ converted.config.js
@@ -1,6 +1,6 @@
 module.exports = {
-  entry: "./src/contracts/", //必传
-  out: "./out", //可不传，默认"./out"
+  entry: "./src/contracts/", //essential
+  out: "./out", //Can not be transferred, default"./out"
   //Contract address of other developers after deployment in other networks
   networks: {
     "3": "0xa6A90c21d6Af4c7008FdB276eE0d9E41636fae3F",
